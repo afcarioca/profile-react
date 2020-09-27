3503dbcbd64c0c3e697cd63894a6b0deecc84644
afcarioca <afcarioca@hotmail.com>
2020-09-27 19:43:52 +0100

    Add Meta Tags

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-
 import Header from "../Header/Hearder.jsx"
 import Main from "../Main/Main.jsx"
 import Footer from "../Footer/Footer.jsx"
+import DocumentMeta from 'react-document-meta';
+
 
 export default class App extends React.Component{
   
   render(){
+    const meta = {
+      title: 'Exercício HTML',
+      
+    };
     return(
-      <div>
-        <Header />
-         <Main />
-        <Footer />
-      </div>
+      <DocumentMeta {...meta}>
+        <div>
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      </DocumentMeta>
+      
     );
   }
 }
