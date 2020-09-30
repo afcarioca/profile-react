6484e56d5353d5620d4ad4e772309bf70c829ac5
afcarioca <afcarioca@hotmail.com>
2020-09-30 10:05:00 +0100

    Add itempro= url

--- src/components/FooterList/FooterList.jsx
+++ src/components/FooterList/FooterList.jsx
@@ -9,19 +9,19 @@ export default class FooterList extends React.Component{
         return(
             <ul className="lista-rodape" itemProp="mainEntity" itemScope itemType="http://schema.org/Person" >
                 <li className="lista-linkedin" itemProp="mainEntityOfPage">
-                    <a  href="https://www.linkedin.com/in/andrefreitas10/ " target="_blank">
+                    <a  href="https://www.linkedin.com/in/andrefreitas10/ " target="_blank" itemProp="url">
                         <img className="linkedin" src={linkedin} itemProp="image" alt="logo-linkedin"/>
                     </a>
                     
                 </li>
                 <li itemProp="mainEntityOfPage">
-                    <a href="https://github.com/afcarioca" target="_blank" >
+                    <a href="https://github.com/afcarioca" target="_blank" itemProp="url">
                         <img className="github" src={github} itemProp="image" alt="logo-github" />
                     </a>
                     
                 </li>
                 <li itemProp="mainEntityOfPage">
-                    <a href="https://facebook.com/andre.gama.92" target="_blank" >
+                    <a href="https://facebook.com/andre.gama.92" target="_blank" itemProp="url">
                         <img className="facebook" src={facebook} itemProp="image" alt="logo-facebook"/>
                     </a>
                     
