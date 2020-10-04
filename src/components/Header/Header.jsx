15ff78144ad837073a238d9c85202b43089a82ef
afcarioca <afcarioca@hotmail.com>
2020-10-04 19:59:52 +0100

    Add Novo Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import "./Header.css"; 
 import profile from "./profile.jpg";
+import java from "./logo-java.jpg";
+import php from "./logo-php.png";
+
 export default class Header extends React.Component{
     render(){
         return(
             <header>
-                <figure itemScope itemType ="http://schema.org/Person">
-                    <img className="foto-profile" src={profile} alt="foto de André" />
-                </figure>
+                <ul>
+                   <li className="header-php">
+                        <a href="/projetos">
+                            
+                            <img className="java" src={php} alt="foto de André" />
+                           
+
+                        </a>
+                   
+                   </li>
+                   <li>
+                        <figure itemScope itemType ="http://schema.org/Person">
+                            <img className="foto-profile" src={profile} alt="foto de André" />
+                        </figure>
+                    </li> 
+                    <li className="header-java">
+                        <a href="/projetos">
+                            <img className="java" src={java} alt="foto de André" />
+                            
+
+                        </a>
+                          
+                    </li>
+                </ul>
+                
              </header>
             
         );
